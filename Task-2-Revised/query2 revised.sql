e9308683eac0a0fce489f3eac4a389713b0f91a5
Samuel-B-G <samuel.bergstengrip@gmail.com>
2026-01-18 16:54:12 +0100

    Add files via upload

---
+++ Task-2-Revised/query2 revised.sql
@@ -0,0 +1,60 @@
+CREATE OR REPLACE VIEW course_instance_allocated_hours AS (
+	SELECT
+		cl.course_code AS "Course Code",
+		ci.course_instance_id AS "Course Instance ID",
+		CONCAT(p.last_name, ', ', p.first_name) AS "Employee Name",
+		jt.job_title AS "Designation",
+		cv.hp AS "HP",
+		ci.study_year AS "Study Year",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Lecture'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Lecture Hours",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Lab'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Lab Hours",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Tutorial'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Tutorial Hours",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Seminar'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Seminar Hours",
+			
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Exam'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Exam Hours",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Admin'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Admin Hours",
+			
+		ROUND(SUM(aa.allocated_hours * ta.factor)::NUMERIC, 2) AS "Total Hours"
+			
+	FROM course_instance ci
+	
+	JOIN course_version cv
+		ON ci.course_version_id = cv.course_version_id
+	JOIN course_layout cl
+		ON cv.course_layout_id = cl.course_layout_id
+	JOIN planned_activity pa
+		ON ci.course_instance_id = pa.course_instance_id
+	JOIN allocated_activity aa
+		ON pa.planned_activity_id = aa.planned_activity_id
+	JOIN employee e
+		ON aa.employment_id = e.employment_id
+	JOIN person p
+		ON e.person_id = p.person_id
+	JOIN job_title jt
+		ON e.job_title_id = jt.job_title_id
+	JOIN teaching_activity ta
+		ON pa.teaching_activity_id = ta.teaching_activity_id
+	WHERE ci.study_year = '2025' AND ci.course_instance_id = 2 -- Specified year and course instance
+	
+	GROUP BY 
+		cl.course_code,
+		e.employment_id,
+		p.first_name,
+		p.last_name,
+		jt.job_title,
+		ci.course_instance_id,
+		ci.study_year,
+		cv.hp
+		
+	ORDER BY p.last_name
+);
+
+-- Run the query below to use the view.
+SELECT *
+FROM course_instance_allocated_hours;
---
+++ Task-2-Revised/query3 revised.sql
@@ -0,0 +1,56 @@
+CREATE OR REPLACE VIEW employee_total_allocated_hours_year AS (
+	SELECT
+		cl.course_code AS "Course Code",
+		ci.course_instance_id AS "Course Instance ID",
+		cv.hp AS "HP",
+		sp.period_name AS "Period",
+		CONCAT(p.last_name, ', ', p.first_name) AS "Employee Name",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Lecture'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Lecture Hours",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Lab'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Lab Hours",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Tutorial'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Tutorial Hours",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Seminar'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Seminar Hours",
+			
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Exam'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Exam Hours",
+		ROUND(SUM(CASE WHEN ta.activity_name = 'Admin'
+			THEN aa.allocated_hours * ta.factor ELSE 0 END)::NUMERIC, 2) AS "Admin Hours",
+			
+		ROUND(SUM(aa.allocated_hours * ta.factor)::NUMERIC, 2) AS "Total Hours"
+	FROM course_instance ci
+	
+	JOIN study_period sp
+		ON ci.study_period_id = sp.study_period_id
+	JOIN course_version cv
+		ON ci.course_version_id = cv.course_version_id
+	JOIN course_layout cl
+		ON cv.course_layout_id = cl.course_layout_id
+	JOIN planned_activity pa
+		ON ci.course_instance_id = pa.course_instance_id
+	JOIN allocated_activity aa
+		ON pa.planned_activity_id = aa.planned_activity_id
+	JOIN employee e
+		ON aa.employment_id = e.employment_id
+	JOIN person p
+		ON e.person_id = p.person_id
+	JOIN teaching_activity ta
+		ON pa.teaching_activity_id = ta.teaching_activity_id
+	WHERE ci.study_year = '2025' AND e.employment_id = 4 -- Specified year and employee id
+	
+	GROUP BY
+		cl.course_code,
+		p.first_name,
+		p.last_name,
+		ci.course_instance_id,
+		sp.period_name,
+		cv.hp
+
+	ORDER BY ci.course_instance_id
+);
+
+-- Run the query below to use the view.
+SELECT *
+FROM employee_total_allocated_hours_year;
